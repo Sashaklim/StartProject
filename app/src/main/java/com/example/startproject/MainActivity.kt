b307185abd5f58a59e9ac2c79eda7c97285d72e3
Sashaklim <sashaklimovic995@gmail.com>
2025-06-19 19:19:43 +0300

    5. Обработка ввода и событий в активности

--- app/src/main/java/com/example/startproject/MainActivity.kt
+++ app/src/main/java/com/example/startproject/MainActivity.kt
@@ -15,5 +15,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        seasonInput = findViewById(R.id.seasonInput)
+        showSeasonButton = findViewById(R.id.showSeasonButton)
+        seasonResult = findViewById(R.id.seasonResult)
+
+        showSeasonButton.setOnClickListener {
+            val input = seasonInput.text.toString().trim()
+            val result = when (input.toIntOrNull()) {
+                1 -> "Зима"
+                2 -> "Весна"
+                3 -> "Лето"
+                4 -> "Осень"
+                else -> "Ошибка: введите число от 1 до 4"
+            }
+            seasonResult.text = result
+        }
     }
 }
