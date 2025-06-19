b47c019aad880c9df67bfbecbeca9a6a9d881cbc
Sashaklim <sashaklimovic995@gmail.com>
2025-06-19 19:03:49 +0300

    2. Добавление текстового поля

--- app/src/main/java/com/example/startproject/MainActivity.kt
+++ app/src/main/java/com/example/startproject/MainActivity.kt
@@ -1,20 +1,19 @@
 package com.example.startproject
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var seasonInput: EditText
+    private lateinit var showSeasonButton: Button
+    private lateinit var seasonResult: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
     }
 }
